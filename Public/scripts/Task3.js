56ddc177aeb2811b387caaf5a26be70f8d7107f5
Vaibhavee Shinde <43693189+VaibhaveeM11@users.noreply.github.com>
2020-07-22 15:16:08 +0530

    code updated

--- Public/scripts/Task3.js
+++ Public/scripts/Task3.js
@@ -3,26 +3,45 @@
 Feature:
 Upload image when user click on button
 Save the uploaed image in folder
-Preview Upload image in div container
+Display Upload image in div container
 */
 $(document).ready(function()
 {
 
-  <img src='./../imahe'></img>
+     
+        // if (objFileInput.files[0]) 
+        // {
+        //     let fileReader = new FileReader();
+        //     fileReader.onload = function (e) {
+        //         $('#blah').attr('src', e.target.result);
+        //         $("#ImgContainer").html('<img src="'+e.target.result+'" width="200px" height="200px" class="upload-preview" />');
+        //         $("#ImgContainer").css('opacity','0.7');
+             
+        //     }
+        //     fileReader.readAsDataURL(objFileInput.files[0]);
+       // }
+    //to upload images
+    
+	$("#btnUpload").click(function() {
+        console.log('rrre');
+		$.ajax({
+        	url: './images/',
+			type: "POST",
+			data:  new FormData(this),
+			beforeSend: function(){$("#body-overlay").show();},
+			contentType: false,
+    	    processData:false,
+			success: function(data)
+		    {
+			$("#ImgContainer").html(data);
+			$("#ImgContainer").css('opacity','1');
+			setInterval(function() {$("#body-overlay").hide(); },500);
+			},
+		  	error: function() 
+	    	{
+	    	} 	        
+	   });
+	});
 
-    $.ajax({
-        url: "./../", // Url to which the request is send
-        type: "POST",             // Type of request to be send, called as method
-        data: new FormData(this), // Data sent to server, a set of key/value pairs (i.e. form fields and values)
-        contentType: false,       // The content type used when sending data to the server.
-        cache: false,             // To unable request pages to be cached
-        processData:false,        // To send DOMDocument or non processed data file it is set to false
-        success: function(data)   // A function to be called if request succeeds
-        {
-        $('#loading').hide();
-        $("#message").html(data);
-        }
-        });
-     // to upload img
-     });
 });
+
